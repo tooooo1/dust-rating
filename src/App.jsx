a9d05ab358e2cfc2a7f0fd2bb4b4e6adee49880d
tooooo1 <wer4272@nate.com>
2022-03-18 15:35:15 +0900

    update : open api 1차 해결!

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ const App = () => {
 
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty'; /*URL*/
-  var queryParams = '?' + encodeURIComponent('serviceKey') + '='+API_KEY; /*Service Key*/
-  queryParams += '&' + encodeURIComponent('stationName') + '=' + encodeURIComponent('종로구'); /**/
-  queryParams += '&' + encodeURIComponent('dataTerm') + '=' + encodeURIComponent('DAILY'); /**/
-  console.log(url+queryParams);
-  axios.get(url + queryParams)
-    
+  var queryParams = '?' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('서울'); /**/
+  queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
+  queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /**/
+  queryParams += '&' + encodeURIComponent('serviceKey') + '=' + API_KEY; /*Service Key*/
+  queryParams += '&' + encodeURIComponent('ver') + '=' + encodeURIComponent('1.0'); /**/
+  console.log(url + queryParams);
+  
+  axios.get("https://cors-anywhere.herokuapp.com/" + url + queryParams)
     .then((response) => {
-      response.setHeader("Access-Control-Allow-Origin", "*");
       const data = response.data;
       setIsLoading(false);
       console.log(data);

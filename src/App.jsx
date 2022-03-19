33bf4483f0cc1424c60fcbdd308f7e55a4bb2f25
tooooo1 <wer4272@nate.com>
2022-03-19 15:16:19 +0900

    feat : react router dom 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,25 @@
-import { useState, useContext } from 'react'
-import axios from "axios";
-import { Dust } from './store/Store'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import { Choice, Result } from "./pages"
+import reset from "styled-reset";
 
-const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
+`;
 
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty'; /*URL*/
-  var queryParams = '?' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('서울'); /**/
-  queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
-  queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /**/
-  queryParams += '&' + encodeURIComponent('serviceKey') + '=' + API_KEY; /*Service Key*/
-  queryParams += '&' + encodeURIComponent('ver') + '=' + encodeURIComponent('1.0'); /**/
-  console.log("https://cors-anywhere.herokuapp.com/" + url + queryParams);
-  
-  axios.get("https://cors-anywhere.herokuapp.com/" + url + queryParams)
-    .then((response) => {
-      const data = response.data;
-      setIsLoading(false);
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  
-  const { dust, cotextDispatch } = useContext(Dust);
-
-
-  return (
-    <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-          <div>
-            <p>{dust.dust}</p>
-            <div>{ dust.ultradust}</div>
-          </div>
-        )}
-    </div>
-  );
-}
+const App = () => (
+    <BrowserRouter>
+      <GlobalStyle />
+      {/* <Logo /> */}
+      <Routes>
+        <Route path="/" element={<Choice />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+);
 
 export default App;

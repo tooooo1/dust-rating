18641835f45dbd820fa85848487b32e57c8e1eb2
tooooo1 <wer4272@nate.com>
2022-03-19 17:29:11 +0900

    update : 각 페이지 기본 레이아웃

--- src/pages/Choice/index.jsx
+++ src/pages/Choice/index.jsx
@@ -1,9 +1,27 @@
-
+import { Positioner } from '../../components/Wrapper/styled'
+import { useNavigate } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Context } from "../../store/Store"
 
 const Choice = () => {
+    let navigate = useNavigate();
+    const { place, contextDispatch } = useContext(Context);
+
     return (
-        <>
-        </>
+        <Positioner>
+            <div>
+                먼지랭킹 {place}
+                <div>
+                    <select>
+                        <option value="서울">서울</option>
+                        <option value="부산" onClick={()=>contextDispatch({type:"부산", value:"부산"})}>부산</option>
+                        <option value="대구">대구</option>
+                    </select>
+                    <button onClick={() => navigate("/result")}>다음</button>
+                    <button onClick={()=>contextDispatch({type:'부산', value:'부산'})}>부산</button>
+                </div>
+            </div>
+        </Positioner>
     )
 };
 
--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,9 +1,22 @@
-
+import { Positioner } from '../../components/Wrapper/styled'
 
 const Result = () => {
     return (
-        <>
-        </>
+        <Positioner>
+            <div>
+                미세먼지 정보
+                <div style={{marginTop:"20px"}}>
+                    미세먼지 : 레벨 1
+                    <div>
+                        초미세먼지 : 레벨 4
+                    </div>
+                </div>
+                <div style={{marginTop:"20px"}}>
+                    총 레이팅 2<br /><br/>
+                    산책하세염~~
+                </div>
+            </div>
+        </Positioner>
     )
 };
 

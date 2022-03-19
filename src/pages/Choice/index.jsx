3150cbb38a6fa23aabeb7503ef63d70afb13d90f
tooooo1 <wer4272@nate.com>
2022-03-20 02:20:12 +0900

    refactor : 시작 화면 리팩토링

--- src/pages/Choice/index.jsx
+++ src/pages/Choice/index.jsx
@@ -2,25 +2,48 @@ import { Positioner } from '../../components/Wrapper/styled'
 import { useNavigate } from "react-router-dom";
 import React, { useContext } from 'react';
 import { Context } from "../../store/Store"
+import * as Styled from './styled';
 
 const Choice = () => {
     let navigate = useNavigate();
     const { place, contextDispatch } = useContext(Context);
+    const city = ['서울', '부산', '대구', '인천', '광주',
+        '대전', '울산', '경기', '강원', '충북', '충남', '전북',
+        '전남', '경북', '경남', '제주', '세종'];
+    
+    // const temp = (lo) => {
+    //     contextDispatch({ value:lo})
+    //     navigate("/result");
+    // }
 
     return (
         <Positioner>
-            <div>
+            <Styled.GlobalStyle />
+            <Styled.Center><img src="img/location.png" alt='location' width={30} /></Styled.Center>
+            <Styled.SubTitle>한 눈에 확인하는</Styled.SubTitle>
+            <Styled.Title>미먼랭킹</Styled.Title>
+            <Styled.Text>미세먼지 농도가 궁금한 지역은?</Styled.Text>
+            <Styled.Center>
+                <Styled.Select>
+                    {
+                        city.map((i) => (
+                            <option value={i} onChange={()=>navigate("/result")}> {i} </option>
+                        ))
+                    }
+                </Styled.Select>
+            </Styled.Center>
+            {/* <div>
                 먼지랭킹 {place}
                 <div>
                     <select>
-                        <option value="서울">서울</option>
-                        <option value="부산" onClick={()=>contextDispatch({type:"부산", value:"부산"})}>부산</option>
+                        <option value="서울" onChange={()=>contextDispatch({ type: "서울", value: "서울" })}>서울</option>
+                        <option value="부산" onChange={()=>contextDispatch({type:"부산", value:"부산"})}>부산</option>
                         <option value="대구">대구</option>
                     </select>
                     <button onClick={() => navigate("/result")}>다음</button>
-                    <button onClick={()=>contextDispatch({type:'부산', value:'부산'})}>부산</button>
+                    <button onClick={()=>contextDispatch({ value:'부산'})}>부산</button>
                 </div>
-            </div>
+            </div> */}
         </Positioner>
     )
 };
--- src/pages/Choice/styled.js
+++ src/pages/Choice/styled.js
@@ -1,2 +1,82 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #53CAF2;
+  }
+  /* #root>div {
+      width: 80%;
+      @media only screen and (min-width: 768px) {
+        width: 70%;
+    }
+  } */
+`;
+
+export const Title = styled.div`
+    font-size : 8vh;
+    font-weight : 1000;
+    margin : 0 0 1.5rem 0;
+    text-align: center;
+    font-family: "Pretendard-Bold";
+    color: white;
+    @media only screen and (min-width: 768px) {
+        font-size: 60px;
+    }
+`;
+
+export const SubTitle = styled.div`
+    font-size : 5vw;
+    margin-bottom : 1.5vh;
+    text-align: center;
+    font-family: "Pretendard-Regular";
+    color: white;
+    @media only screen and (min-width: 768px) {
+        font-size: 20px;
+    }
+`;
+
+export const Text = styled.div`
+    font-size : 4vw;
+    margin-bottom : 1.5vh;
+    text-align: center;
+    font-family: "Pretendard-SemiBold";
+    color: white;
+    &#last {
+        margin-bottom: 4vh;
+    }
+    @media only screen and (min-width: 768px) {
+        font-size: 20px;
+    }
+`;
+
+export const Center = styled.div`
+    text-align: center;
+    margin-bottom: 1rem;
+`;
+
+export const Select = styled.select`
+    height: 6vh;
+    color: #9DADB6;
+
+    font-weight: bold;
+    font-family: "Pretendard-Regular";
+    
+    opacity: 0.8;
+    /* -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none; */
+    padding-right: 1rem;
+    padding-left: 1rem;
+    border-radius: 10px;
+    border: none;
+    font-size: 16px;
+
+    &:focus-visible {
+        outline: white solid 2px;
+    }
+
+    option {
+        border-radius: 8px;
+        color: black;
+    }
+`;

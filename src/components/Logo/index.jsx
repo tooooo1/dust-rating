8d8ad68c8a45391b2ebd1741655f7fc4d07e5a0e
tooooo1 <wer4272@nate.com>
2022-03-24 04:08:28 +0900

    feat : logo 추가

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './styled';
+
+const Logo = () => (
+    <Styled.Img>
+        <a href='https://tooo1.tistory.com'>
+            <img src="img/il.jpg" alt='il' width={50} style={{ borderRadius: "100px" }} />
+        </a>
+    </Styled.Img>
+);
+
+export default Logo;
---
+++ src/components/Logo/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Img = styled.div`
+    position: absolute;
+    top : 10px;
+    left : 10px;
+    z-index: 1;
+`;

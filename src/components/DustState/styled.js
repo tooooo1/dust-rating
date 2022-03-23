dddbdac2644e9bc5e7b63937f033436bc336ecc0
tooooo1 <wer4272@nate.com>
2022-03-24 02:42:45 +0900

    feat : 먼지 상태 component 추가

---
+++ src/components/DustState/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+
+export const DustStateColor = styled.div`
+    font-size : 5.5vw;
+    display: flex;
+    justify-content: center;
+    font-family: "Pretendard-Bold";
+    @media only screen and (min-width: 768px) {
+        font-size: 25px;
+    }
+`;

abdd818ea507bbe0e3d405b5622f77cbc31e6f38
tooooo1 <wer4272@nate.com>
2022-12-03 18:58:23 +0900

    style: en -> ko, style remove

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
 import Store from './store/Store';
 
 ReactDOM.render(
@@ -15,8 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

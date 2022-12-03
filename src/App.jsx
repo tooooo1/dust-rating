5a7c5e417501041aee4badbbe34ffaaf5f88df2a
tooooo1 <wer4272@nate.com>
2022-12-03 18:57:25 +0900

    remove: web-vitals and setting change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
-import Logo from './components/Logo/index.jsx';
+import Logo from './components/Logo.jsx';
 import Choice from './pages/Choice.jsx';
 import Result from './pages/Result.jsx';
 
@@ -12,6 +12,29 @@ const GlobalStyle = createGlobalStyle`
     outline: none;
     box-sizing: border-box;
   }
+  body {
+    background-color: #53CAF2;
+  }
+  #root {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Pretendard-SemiBold', sans-serif;
+  }
+  #root>div {
+    @media all and (min-width:1024px) { 
+        width: 600px;
+    }
+    @media all and (min-width:768px) and (max-width:1023px) {
+        width: 600px;
+    }
+    @media all and (max-width:767px) {
+        width: 100%
+    }
+  }
+
 `;
 
 const App = () => (

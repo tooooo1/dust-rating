7ed1f9c96d87f2b8e77ae3d0a402a2b340697409
tooooo1 <wer4272@nate.com>
2022-12-08 13:54:54 +0900

    feat: custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const OPEN_URL = process.env.REACT_APP_OPEN_URL;
+
+export const cityGroup = [
+  { cityName: '서울', cityNumber: 0 },
+  { cityName: '부산', cityNumber: 1 },
+  { cityName: '대구', cityNumber: 2 },
+  { cityName: '인천', cityNumber: 3 },
+  { cityName: '광주', cityNumber: 4 },
+  { cityName: '대전', cityNumber: 5 },
+  { cityName: '울산', cityNumber: 6 },
+  { cityName: '경기', cityNumber: 7 },
+  { cityName: '강원', cityNumber: 8 },
+  { cityName: '충북', cityNumber: 9 },
+  { cityName: '충남', cityNumber: 10 },
+  { cityName: '전북', cityNumber: 11 },
+  { cityName: '전남', cityNumber: 12 },
+  { cityName: '경북', cityNumber: 13 },
+  { cityName: '경남', cityNumber: 14 },
+  { cityName: '제주', cityNumber: 15 },
+  { cityName: '세종', cityNumber: 16 },
+];
+
+const useFetch = async () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    setData(
+      Promise.all(
+        cityGroup.map((v) =>
+          axios
+            .get(
+              `${OPEN_URL}?sidoName=${v.cityName}&pageNo=1&numOfRows=100&returnType=json&serviceKey=${API_KEY}&ver=1.0`
+            )
+            .then((data) => data.data.response.body)
+        )
+      ).then((data) => data)
+    );
+  }, []);
+
+  return data;
+};
+
+export default useFetch;

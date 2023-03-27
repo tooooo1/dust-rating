23aef715ea1a3754d8d7e8c696355dc6420850d3
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-27 12:32:38 +0900

    refactor: as 단언문 제거

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+try {
+  const rootElement = document.getElementById('root2');
+  if (!rootElement) throw new Error('root element not found!');
+
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} catch (err) {
+  console.error(err);
+}

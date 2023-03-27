e7a49157e49c6e7fc1226f973467b888aa3b6154
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-27 19:35:43 +0900

    refactor: pr 리뷰 반영

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-try {
-  const rootElement = document.getElementById('root2');
-  if (!rootElement) throw new Error('root element not found!');
-
-  const root = ReactDOM.createRoot(rootElement);
-
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} catch (err) {
-  console.error(err);
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

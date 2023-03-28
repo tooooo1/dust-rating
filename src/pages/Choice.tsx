fa2e2f382ad292136e1f4d00263c4810ab8642d7
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-28 19:21:14 +0900

    refactor: Choice 컴포넌트 tsx로 변경

--- src/pages/Choice.tsx
+++ src/pages/Choice.tsx
@@ -1,20 +1,39 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-import Button from '../components/Button.jsx';
-import { cityGroup } from '../hooks/useFetch.js';
+import Button from '@/components/Button';
+
+const cityGroup = [
+  { cityName: '서울', cityNumber: 0 },
+  { cityName: '부산', cityNumber: 1 },
+  { cityName: '대구', cityNumber: 2 },
+  { cityName: '인천', cityNumber: 3 },
+  { cityName: '광주', cityNumber: 4 },
+  { cityName: '대전', cityNumber: 5 },
+  { cityName: '울산', cityNumber: 6 },
+  { cityName: '경기', cityNumber: 7 },
+  { cityName: '강원', cityNumber: 8 },
+  { cityName: '충북', cityNumber: 9 },
+  { cityName: '충남', cityNumber: 10 },
+  { cityName: '전북', cityNumber: 11 },
+  { cityName: '전남', cityNumber: 12 },
+  { cityName: '경북', cityNumber: 13 },
+  { cityName: '경남', cityNumber: 14 },
+  { cityName: '제주', cityNumber: 15 },
+  { cityName: '세종', cityNumber: 16 },
+];
 
 const Choice = () => {
   const navigate = useNavigate();
   const [place, setPlace] = useState('서울');
 
-  const onClick = () => {
+  const handleClick = () => {
     navigate('/result', { state: place });
   };
 
-  const onChangeHandler = (e) => {
-    setPlace(e.currentTarget.value);
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setPlace(e.target.value);
   };
 
   return (
@@ -23,14 +42,14 @@ const Choice = () => {
       <SubTitle>한 눈에 확인하는</SubTitle>
       <Title>랭킹먼지</Title>
       <Text>미세먼지 농도가 궁금한 지역은?</Text>
-      <Select onChange={onChangeHandler} value={place}>
+      <Select onChange={handleChange} value={place}>
         {cityGroup.map((v) => (
           <option key={v.cityName} value={v.cityName}>
             {v.cityName}
           </option>
         ))}
       </Select>
-      <Button color={'#2886A6'} onClick={() => onClick()}>
+      <Button color={'#2886A6'} onClick={handleClick}>
         검색
       </Button>
     </Wrapper>
@@ -51,7 +70,6 @@ const Title = styled.div`
   font-size: 14vw;
   padding-bottom: 1.5rem;
   text-align: center;
-  font-family: 'Pretendard-Bold';
   @media only screen and (min-width: 768px) {
     font-size: 60px;
   }
@@ -61,7 +79,6 @@ const SubTitle = styled.div`
   font-size: 5vw;
   padding: 1rem 0;
   text-align: center;
-  font-family: 'Pretendard-Regular';
   @media only screen and (min-width: 768px) {
     font-size: 20px;
   }
@@ -71,7 +88,6 @@ const Text = styled.div`
   font-size: 3.5vw;
   margin-bottom: 1.5vh;
   text-align: center;
-  font-family: 'Pretendard-SemiBold';
   @media only screen and (min-width: 768px) {
     font-size: 20px;
   }
@@ -81,7 +97,6 @@ const Select = styled.select`
   height: 6vh;
   margin-bottom: 1rem;
   font-weight: bold;
-  font-family: 'Pretendard-Medium';
   background-color: white;
   opacity: 0.8;
   -moz-appearance: none;

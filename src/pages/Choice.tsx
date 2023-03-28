5715b86546672823fd7f4f1297b833f6b9c8130a
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-28 19:50:24 +0900

    refactor: vite-env 설정 및 useFetch의 cityGroup 변수 사용

--- src/pages/Choice.tsx
+++ src/pages/Choice.tsx
@@ -3,26 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Button from '@/components/Button';
-
-const cityGroup = [
-  { cityName: '서울', cityNumber: 0 },
-  { cityName: '부산', cityNumber: 1 },
-  { cityName: '대구', cityNumber: 2 },
-  { cityName: '인천', cityNumber: 3 },
-  { cityName: '광주', cityNumber: 4 },
-  { cityName: '대전', cityNumber: 5 },
-  { cityName: '울산', cityNumber: 6 },
-  { cityName: '경기', cityNumber: 7 },
-  { cityName: '강원', cityNumber: 8 },
-  { cityName: '충북', cityNumber: 9 },
-  { cityName: '충남', cityNumber: 10 },
-  { cityName: '전북', cityNumber: 11 },
-  { cityName: '전남', cityNumber: 12 },
-  { cityName: '경북', cityNumber: 13 },
-  { cityName: '경남', cityNumber: 14 },
-  { cityName: '제주', cityNumber: 15 },
-  { cityName: '세종', cityNumber: 16 },
-];
+import { cityGroup } from '@/hooks/useFetch';
 
 const Choice = () => {
   const navigate = useNavigate();

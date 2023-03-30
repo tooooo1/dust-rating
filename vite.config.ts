6e343c4dd17fa3ffa5320596180087bfc691f477
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-30 17:54:39 +0900

    chore: chakra-ui vite 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },

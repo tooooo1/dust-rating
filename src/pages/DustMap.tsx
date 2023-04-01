802f54e94299f8a294f736d6394919fb24dce05a
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-01 20:45:31 +0900

    feat: 초기 위치 및 현위치 값 설정

--- src/pages/DustMap.tsx
+++ src/pages/DustMap.tsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from 'react';
+import Map from '@/components/Map';
+import styled from '@emotion/styled';
+
+const DEFAULT_LOC = { latitude: 37.5665, longitude: 126.978 };
+
 const DustMapPage = () => {
-  return <div></div>;
+  const [latitude, setLatitude] = useState(DEFAULT_LOC.latitude);
+  const [longitude, setLongitude] = useState(DEFAULT_LOC.longitude);
+
+  useEffect(() => {
+    navigator.geolocation &&
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      });
+  }, []);
+
+  return (
+    <MapContainer>
+      <Map latitude={latitude} longitude={longitude} />
+    </MapContainer>
+  );
 };
 
 export default DustMapPage;
+
+const MapContainer = styled.div`
+  max-width: 520px;
+  width: 100vw;
+  height: 100vh;
+`;

1859ca2de70a1404a4e786ed4e87b1fab037cbf3
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-01 20:38:32 +0900

    feat: DustMap 페이지 라우터 설정

---
+++ src/pages/DustMap.tsx
@@ -0,0 +1,5 @@
+const DustMapPage = () => {
+  return <div></div>;
+};
+
+export default DustMapPage;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as ChoicePage } from '@/pages/Choice';
 export { default as ResultPage } from '@/pages/Result';
+export { default as DustMapPage } from '@/pages/DustMap';

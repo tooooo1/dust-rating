e4f21d4b764ea6a2ed953bbee053f9a253ec0d29
JaeKwan Ahn <52008@naver.com>
2023-04-03 01:53:57 +0900

    refactor: type -> interface로 변경

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,4 +1,4 @@
-export type DustData = {
+export interface DustData {
   items: {
     dataTime: string;
     stationName: string;
@@ -11,21 +11,21 @@ export type DustData = {
   numOfRows: number;
   totalCount: number;
   pageNo: number;
-};
+}
 
-export type Detail = {
+export interface Detail {
   dataTime: string;
   stationName: string;
   pm10Value: string;
   pm25Value: string;
   pm10Grade: string;
   pm25Grade: string;
-};
+}
 
-export type LocalDustDetail = {
+export interface LocalDustDetail {
   stationName: string;
   fineDust: string;
   ultraDust: string;
   dataTime: string;
   dustState: string;
-};
+}

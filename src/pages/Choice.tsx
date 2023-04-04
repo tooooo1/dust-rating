187bd5a55081582342a9c334690ead2dd1dba1f0
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-05 01:40:36 +0900

    refactor: select 태그에 chakra 적용

--- src/pages/Choice.tsx
+++ src/pages/Choice.tsx
@@ -2,8 +2,7 @@ import { ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { cityGroup } from '@/hooks/useFetch';
 
-import styled from '@emotion/styled';
-import { Flex, Text, IconButton } from '@chakra-ui/react';
+import { Flex, Text, Select, IconButton } from '@chakra-ui/react';
 import { HiArrowRight } from 'react-icons/hi';
 
 const Choice = () => {
@@ -36,54 +35,36 @@ const Choice = () => {
       <Text as="span" fontSize={18} fontWeight={400} lineHeight="2rem">
         미세먼지 농도가 궁금한 지역은?
       </Text>
-      <Select onChange={handlePlaceChange} value={place}>
-        {cityGroup.map((v) => (
-          <Option key={v.cityName} value={v.cityName}>
-            {v.cityName}
-          </Option>
-        ))}
-      </Select>
-      <IconButton
-        aria-label="search"
-        color="#fff"
-        backgroundColor="#3a9cbd"
-        px={6}
-        borderRadius={20}
-        icon={<HiArrowRight />}
-        onClick={handleResultPageNavigate}
-        _hover={{ bg: '#2886A6' }}
-      />
+      <Flex gap={4} mt={6}>
+        <Select
+          variant="filled"
+          width={28}
+          bg="#ffffff"
+          color="#2a282f"
+          cursor="pointer"
+          _focus={{ bg: '#ffffff' }}
+          onChange={handlePlaceChange}
+          value={place}
+        >
+          {cityGroup.map((city) => (
+            <option key={city.cityName} value={city.cityName}>
+              {city.cityName}
+            </option>
+          ))}
+        </Select>
+        <IconButton
+          aria-label="search"
+          color="#ffffff"
+          bg="#3a9cbd"
+          px={6}
+          borderRadius={20}
+          icon={<HiArrowRight />}
+          onClick={handleResultPageNavigate}
+          _hover={{ bg: '#2886A6' }}
+        />
+      </Flex>
     </Flex>
   );
 };
 
 export default Choice;
-
-const Select = styled.select`
-  margin: 1rem 0;
-  font-weight: 600;
-  background-color: #fff;
-  opacity: 0.8;
-  color: #2a282f;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  appearance: none;
-  padding: 1rem 1.1rem;
-  border-radius: 0.5rem;
-  border: none;
-  font-size: 1rem;
-  cursor: pointer;
-  transition: all 0.3s;
-
-  &:hover {
-    opacity: 1;
-  }
-
-  &:focus-visible {
-    outline: 0.1rem solid #fff;
-  }
-`;
-
-const Option = styled.option`
-  border-radius: 0.5rem;
-`;

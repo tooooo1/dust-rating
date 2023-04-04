5719fa5a2b407d325c6e273180c5a90c4926120a
JaeKwan Ahn <52008@naver.com>
2023-04-04 17:43:47 +0900

    refactor: translate 파일 위치 변경, 변수 선언 키워드 변경

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -7,37 +7,37 @@
 //
 // LCC DFS 좌표변환을 위한 기초 자료
 //
-var RE = 6371.00877; // 지구 반경(km)
-var GRID = 5.0; // 격자 간격(km)
-var SLAT1 = 30.0; // 투영 위도1(degree)
-var SLAT2 = 60.0; // 투영 위도2(degree)
-var OLON = 126.0; // 기준점 경도(degree)
-var OLAT = 38.0; // 기준점 위도(degree)
-var XO = 43; // 기준점 X좌표(GRID)
-var YO = 136; // 기1준점 Y좌표(GRID)
+const GRID = 5.0; // 격자 간격(km)
+const SLAT1 = 30.0; // 투영 위도1(degree)
+const SLAT2 = 60.0; // 투영 위도2(degree)
+const RE = 6371.00877; // 지구 반경(km)
+const OLON = 126.0; // 기준점 경도(degree)
+const OLAT = 38.0; // 기준점 위도(degree)
+const XO = 43; // 기준점 X좌표(GRID)
+const YO = 136; // 기1준점 Y좌표(GRID)
 //
 // LCC DFS 좌표변환 ( code : "toXY"(위경도->좌표, v1:위도, v2:경도), "toLL"(좌표->위경도,v1:x, v2:y) )
 //
 
-export function dfs_xy_conv(code, v1, v2) {
-  var DEGRAD = Math.PI / 180.0;
-  var RADDEG = 180.0 / Math.PI;
+export const convertGridValue = (code: string, v1: number, v2: number) => {
+  const DEGRAD = Math.PI / 180.0;
+  const RADDEG = 180.0 / Math.PI;
 
-  var re = RE / GRID;
-  var slat1 = SLAT1 * DEGRAD;
-  var slat2 = SLAT2 * DEGRAD;
-  var olon = OLON * DEGRAD;
-  var olat = OLAT * DEGRAD;
+  const re = RE / GRID;
+  const slat1 = SLAT1 * DEGRAD;
+  const slat2 = SLAT2 * DEGRAD;
+  const olon = OLON * DEGRAD;
+  const olat = OLAT * DEGRAD;
 
-  var sn =
+  let sn =
     Math.tan(Math.PI * 0.25 + slat2 * 0.5) /
     Math.tan(Math.PI * 0.25 + slat1 * 0.5);
   sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
-  var sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+  let sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
   sf = (Math.pow(sf, sn) * Math.cos(slat1)) / sn;
-  var ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+  let ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
   ro = (re * sf) / Math.pow(ro, sn);
-  var rs = {};
+  const rs: { [key: string]: number } = {};
   if (code == 'toXY') {
     rs['lat'] = v1;
     rs['lng'] = v2;
@@ -52,11 +52,11 @@ export function dfs_xy_conv(code, v1, v2) {
   } else {
     rs['x'] = v1;
     rs['y'] = v2;
-    var xn = v1 - XO;
-    var yn = ro - v2 + YO;
+    const xn = v1 - XO;
+    const yn = ro - v2 + YO;
     ra = Math.sqrt(xn * xn + yn * yn);
     if (sn < 0.0) -ra;
-    var alat = Math.pow((re * sf) / ra, 1.0 / sn);
+    let alat = Math.pow((re * sf) / ra, 1.0 / sn);
     alat = 2.0 * Math.atan(alat) - Math.PI * 0.5;
 
     if (Math.abs(xn) <= 0.0) {
@@ -67,11 +67,11 @@ export function dfs_xy_conv(code, v1, v2) {
         if (xn < 0.0) -theta;
       } else theta = Math.atan2(xn, yn);
     }
-    var alon = theta / sn + olon;
+    const alon = theta / sn + olon;
     rs['lat'] = alat * RADDEG;
     rs['lng'] = alon * RADDEG;
   }
 
   return rs;
-}
-export default dfs_xy_conv;
+};
+export default convertGridValue;

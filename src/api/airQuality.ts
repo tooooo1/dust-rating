79fd19792acebe6e524f8581f740ec88b6920e6c
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-08 17:02:01 +0900

    refactor: 미세먼지, 초미세먼지 api 분리 및 데이터 정제

---
+++ src/api/airQuality.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const { VITE_AIR_QUALITY_URL, VITE_AIR_QUALITY_API_KEY } = import.meta.env;
+
+export const CITY_GROUP = [
+  { cityName: '서울', cityNumber: 0 },
+  { cityName: '부산', cityNumber: 1 },
+  { cityName: '대구', cityNumber: 2 },
+  { cityName: '인천', cityNumber: 3 },
+  { cityName: '광주', cityNumber: 4 },
+  { cityName: '대전', cityNumber: 5 },
+  { cityName: '울산', cityNumber: 6 },
+  { cityName: '경기', cityNumber: 7 },
+  { cityName: '강원', cityNumber: 8 },
+  { cityName: '충북', cityNumber: 9 },
+  { cityName: '충남', cityNumber: 10 },
+  { cityName: '전북', cityNumber: 11 },
+  { cityName: '전남', cityNumber: 12 },
+  { cityName: '경북', cityNumber: 13 },
+  { cityName: '경남', cityNumber: 14 },
+  { cityName: '제주', cityNumber: 15 },
+  { cityName: '세종', cityNumber: 16 },
+];
+
+export const getAirQuality = async () => {
+  try {
+    const result = await Promise.all(
+      CITY_GROUP.map((city) =>
+        axios
+          .get(
+            `${VITE_AIR_QUALITY_URL}?sidoName=${city.cityName}&pageNo=1&numOfRows=100&returnType=json&serviceKey=${VITE_AIR_QUALITY_API_KEY}&ver=1.0`
+          )
+          .then((res) => {
+            return res.data.response.body;
+          })
+      )
+    );
+
+    const airQuality = CITY_GROUP.map((city) => ({
+      cityName: city.cityName,
+      fineDustScale: Number(result[city.cityNumber]?.items[4]?.pm10Value),
+      ultraFineDustScale: Number(result[city.cityNumber]?.items[4]?.pm25Value),
+    }));
+
+    return airQuality;
+  } catch (error) {
+    console.error(error);
+  }
+};

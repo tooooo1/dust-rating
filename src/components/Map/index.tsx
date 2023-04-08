0cd66eba4a636d711c7a6f657c085bd0daa1ef9e
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-08 18:00:02 +0900

    feat: 지역별 미세먼지, 초미세먼지 마커 띄우기

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,9 @@
-import { VStack, Box } from '@chakra-ui/react';
 import { useEffect, useRef, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import MapButton from './MapButton';
+import DustInfo from './DustInfo';
+import { VStack, Box, Spinner } from '@chakra-ui/react';
+import { getAirQuality } from '@/api/airQuality';
 import { INIT_LOCATION } from '@/utils/constance';
 
 declare global {
@@ -9,10 +12,14 @@ declare global {
   }
 }
 
+const MAX_ZOOM_LEVEL = 13;
+const INIT_ZOOM_LEVEL = 5;
+
 const Map = () => {
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
   const [location, setLocation] = useState(INIT_LOCATION);
+  const [zoomLevel, setZoomLevel] = useState(INIT_ZOOM_LEVEL);
 
   useEffect(() => {
     kakao.maps.load(() => {
@@ -20,7 +27,7 @@ const Map = () => {
 
       const options = {
         center: new kakao.maps.LatLng(location.latitude, location.longitude),
-        level: 5,
+        level: INIT_ZOOM_LEVEL,
       };
 
       const kakaoMap = new kakao.maps.Map(mapRef.current, options);
@@ -45,11 +52,26 @@ const Map = () => {
           kakaoMap.setCenter(new kakao.maps.LatLng(latitude, longitude));
         });
 
-      kakaoMap.setMaxLevel(10);
+      kakaoMap.setMaxLevel(MAX_ZOOM_LEVEL);
 
       setMap(kakaoMap);
     });
   }, []);
+
+  useEffect(() => {
+    kakao.maps.load(() => {
+      if (!map) return;
+
+      kakao.maps.event.addListener(map, 'zoom_changed', () => {
+        const zoomLevel = map.getLevel();
+        setZoomLevel(zoomLevel);
+      });
+    });
+  }, [map]);
+
+  const { data, isLoading } = useQuery(['airQuality'], getAirQuality, {
+    enabled: zoomLevel === MAX_ZOOM_LEVEL,
+  });
 
   const handleCurrentLocationChange = () => {
     kakao.maps.load(() => {
@@ -79,6 +101,14 @@ const Map = () => {
     });
   };
 
+  const handleFullScreenChange = () => {
+    kakao.maps.load(() => {
+      if (!map) return;
+
+      map.setLevel(MAX_ZOOM_LEVEL, { animate: true });
+    });
+  };
+
   return (
     <Box position="relative" width="100%" height="100%">
       <div
@@ -95,6 +125,19 @@ const Map = () => {
         />
         <MapButton type="zoom-in" onClick={handleZoomIn} />
         <MapButton type="zoom-out" onClick={handleZoomOut} />
+        <MapButton type="full-screen" onClick={handleFullScreenChange} />
+        {zoomLevel === MAX_ZOOM_LEVEL && isLoading && <Spinner />}
+        {zoomLevel === MAX_ZOOM_LEVEL &&
+          !isLoading &&
+          data &&
+          data.map((v) => (
+            <DustInfo
+              key={v.cityName}
+              area={v.cityName}
+              fineDust={v.fineDustScale}
+              ultraFineDust={v.ultraFineDustScale}
+            />
+          ))}
       </VStack>
     </Box>
   );

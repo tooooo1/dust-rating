52b00d2c7e6e9c962bc8eb56a68f2eb5fd232eda
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-12 19:04:14 +0900

    fix: 유효한 값 반환하도록 수정

--- src/api/airQuality.ts
+++ src/api/airQuality.ts
@@ -5,23 +5,33 @@ const { VITE_AIR_QUALITY_URL, VITE_AIR_QUALITY_API_KEY } = import.meta.env;
 
 export const getAirQuality = async () => {
   try {
-    const result = await Promise.all(
-      CITY_GROUP.map((city) =>
-        axios
-          .get(
-            `${VITE_AIR_QUALITY_URL}?sidoName=${city.cityName}&pageNo=1&numOfRows=100&returnType=json&serviceKey=${VITE_AIR_QUALITY_API_KEY}&ver=1.0`
-          )
-          .then((res) => {
-            return res.data.response.body;
-          })
-      )
-    );
+    return await Promise.all(
+      CITY_GROUP.map(async (city) => {
+        const response = await axios.get(
+          `${VITE_AIR_QUALITY_URL}?sidoName=${city.cityName}&pageNo=1&numOfRows=100&returnType=json&serviceKey=${VITE_AIR_QUALITY_API_KEY}&ver=1.0`
+        );
 
-    return CITY_GROUP.map((city) => ({
-      cityName: city.cityName,
-      fineDustScale: Number(result[city.cityNumber]?.items[3]?.pm10Value),
-      ultraFineDustScale: Number(result[city.cityNumber]?.items[3]?.pm25Value),
-    }));
+        let fineDustScale = 0;
+        let ultraFineDustScale = 0;
+
+        for (const airQuality of response.data.response.body.items) {
+          if (airQuality.pm10Flag !== null || airQuality.pm25Flag !== null) {
+            continue;
+          }
+
+          fineDustScale = Number(airQuality.pm10Value);
+          ultraFineDustScale = Number(airQuality.pm25Value);
+
+          break;
+        }
+
+        return {
+          cityName: city.cityName,
+          fineDustScale,
+          ultraFineDustScale,
+        };
+      })
+    );
   } catch (error) {
     console.error(error);
   }

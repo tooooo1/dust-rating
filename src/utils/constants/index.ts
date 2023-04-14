d7e779382040aa380d0a1be588725b34fa26d62a
JaeKwan Ahn <52008@naver.com>
2023-04-15 00:56:10 +0900

    Refactor: 기존 상수들 위치 변경

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,3 @@
+export { CITY_GROUP } from '@/utils/constants/city';
+export { INIT_LOCATION, CENTER_LOCATION } from '@/utils/constants/location';
+export { FINE_DUST, ULTRA_FINE_DUST } from '@/utils/constants/dust';

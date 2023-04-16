f06ab63b8ed96d0a398977bfb2e4197a931f7844
JaeKwan Ahn <52008@naver.com>
2023-04-16 12:33:52 +0900

    Refactor: 미세먼지 기상정보 url 환경 변수화

--- src/hooks/useFetchDustForecast.ts
+++ src/hooks/useFetchDustForecast.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-const { VITE_AIR_QUALITY_API_KEY } = import.meta.env;
+const { VITE_AIR_QUALITY_API_KEY, VITE_MINU_DUST_FRCST_DSPTH_URL } = import.meta
+  .env;
 
 export const useFetchDustForecast = () => {
   const [dustForecase, setDustForecast] = useState('기상예보를 준비중 입니다.');
@@ -23,7 +24,7 @@ export const useFetchDustForecast = () => {
     try {
       const forecast = await axios
         .get(
-          `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=${getToday()}&returnType=json&serviceKey=${VITE_AIR_QUALITY_API_KEY}&numOfRows=100&pageNo=1`
+          `${VITE_MINU_DUST_FRCST_DSPTH_URL}?searchDate=${getToday()}&returnType=json&serviceKey=${VITE_AIR_QUALITY_API_KEY}&numOfRows=100&pageNo=1`
         )
         .then((res) => res.data.response.body.items[0].informOverall);
       setDustForecast(forecast);

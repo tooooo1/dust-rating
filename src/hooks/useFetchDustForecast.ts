e2c2151c0ac666524b96f6c3472da2a9c6c6266a
JaeKwan Ahn <52008@naver.com>
2023-04-18 15:50:40 +0900

    Refactor: hook 외부로 getToday함수 이동

--- src/hooks/useFetchDustForecast.ts
+++ src/hooks/useFetchDustForecast.ts
@@ -1,23 +1,22 @@
 import axios from 'axios';
-import { useState } from 'react';
 
 const { VITE_AIR_QUALITY_API_KEY, VITE_MINU_DUST_FRCST_DSPTH_URL } = import.meta
   .env;
 
+const getToday = () => {
+  const date = new Date();
+  const year = date.getFullYear().toString();
+  const month =
+    date.getMonth() < 10
+      ? '0' + (date.getMonth() + 1)
+      : (date.getMonth() + 1).toString();
+  const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  const today = year + '-' + month + '-' + day;
+
+  return today;
+};
+
 export const useFetchDustForecast = () => {
-  const getToday = () => {
-    const date = new Date();
-    const year = date.getFullYear().toString();
-    const month =
-      date.getMonth() < 10
-        ? '0' + (date.getMonth() + 1)
-        : (date.getMonth() + 1).toString();
-    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
-    const today = year + '-' + month + '-' + day;
-
-    return today;
-  };
-
   const fetchDustForecast = async () => {
     try {
       const forecast = await axios

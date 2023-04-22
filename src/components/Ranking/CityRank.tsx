4f74294d7eea39ad0d7e96358126ca0fe6752bfc
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-22 19:22:04 +0900

    refactor: RankItem 컴포넌트 분리

--- src/components/Ranking/CityRank.tsx
+++ src/components/Ranking/CityRank.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
-import { Box, Flex, Text } from '@chakra-ui/react';
-import { DustState } from '@/components/Dust';
+import { Box } from '@chakra-ui/react';
+import RankItem from './RankItem';
 import { getCityAirQualities } from '@/api/airQuality';
-import { FINE_DUST, ULTRA_FINE_DUST } from '@/utils/constants';
 import type { CityAirQuality } from '@/type';
 
 interface CityRankProps {
@@ -31,70 +30,16 @@ const CityRank = ({ sido, isShow }: CityRankProps) => {
       {isShow &&
         cityAirQualities &&
         cityAirQualities.map((city, cityIndex) => (
-          <Flex
+          <RankItem
             key={city.cityName}
-            justifyContent="space-between"
-            alignItems="center"
-            py={3}
-          >
-            <Text
-              as="span"
-              fontSize={18}
-              fontWeight={400}
-              mr={4}
-              color="#9dadb6"
-            >
-              {cityIndex + 1}
-            </Text>
-            <Text
-              as="p"
-              width="30%"
-              fontSize={24}
-              fontWeight={500}
-              overflow="hidden"
-              whiteSpace="nowrap"
-              textOverflow="ellipsis"
-            >
-              {city.cityName}
-            </Text>
-            <DustState
-              fineDust={city.fineDustGrade}
-              ultraFineDust={city.ultraFineDustGrade}
-              kindOfDust="avg"
-            />
-            <Flex direction="column" justifyContent="center" flexGrow={1}>
-              <Flex justifyContent="space-between" py={1}>
-                <Text
-                  as="p"
-                  fontSize={16}
-                  fontWeight={400}
-                  overflow="hidden"
-                  whiteSpace="nowrap"
-                  textOverflow="ellipsis"
-                >
-                  {FINE_DUST}
-                </Text>
-                <Text as="p" fontSize={16} fontWeight={600}>
-                  {city.fineDustScale}
-                </Text>
-              </Flex>
-              <Flex justifyContent="space-between" py={1}>
-                <Text
-                  as="p"
-                  fontSize={16}
-                  fontWeight={400}
-                  overflow="hidden"
-                  whiteSpace="nowrap"
-                  textOverflow="ellipsis"
-                >
-                  {ULTRA_FINE_DUST}
-                </Text>
-                <Text as="p" fontSize={16} fontWeight={600}>
-                  {city.ultraFineDustScale}
-                </Text>
-              </Flex>
-            </Flex>
-          </Flex>
+            cityName={city.cityName}
+            rank={cityIndex + 1}
+            fineDustScale={city.fineDustScale}
+            fineDustGrade={city.fineDustGrade}
+            ultraFineDustScale={city.ultraFineDustScale}
+            ultraFineDustGrade={city.ultraFineDustGrade}
+            dataTime={city.dataTime}
+          />
         ))}
     </Box>
   );

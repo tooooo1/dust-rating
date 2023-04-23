58834998f5023a766286cc6088b2d742d3672c80
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-04-23 22:30:37 +0900

    refactor: getTodayDate formater 함수 utils로 이동

---
+++ src/utils/formaters.ts
@@ -0,0 +1,10 @@
+export const getTodayDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}-${month < 10 ? '0' + month : month}-${
+    day < 10 ? '0' + day : day
+  }`;
+};

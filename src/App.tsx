8a7b039706e9074db690c07cc4b6981b0dd84b66
JaeKwan Ahn <52008@naver.com>
2023-05-03 03:01:21 +0900

    Refactor: Error 페이지 제거, HOME으로 리디렉션

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Navigate,
 } from 'react-router-dom';
 
 import Logo from '@/components/Logo';
@@ -11,17 +12,17 @@ import {
   RankingPage,
   DustForecastPage,
   DustMapPage,
-  ErrorPage,
 } from '@/pages';
 import { ROUTE } from '@/utils/constants';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path={ROUTE.HOME} element={<Logo />} errorElement={<ErrorPage />}>
+    <Route path={ROUTE.HOME} element={<Logo />}>
       <Route index element={<ChoicePage />} />
       <Route path={ROUTE.RANKING} element={<RankingPage />} />
       <Route path={ROUTE.DUST_FORECAST} element={<DustForecastPage />} />
       <Route path={ROUTE.DUST_MAP} element={<DustMapPage />} />
+      <Route path="*" element={<Navigate replace to={ROUTE.HOME} />} />
     </Route>
   )
 );
--- src/pages/ErrorBoundary.tsx
+++
@@ -1,6 +0,0 @@
-import ErrorBoundary from '@/components/Error/CustomError';
-
-export const ErrorCoustom = () => {
-  return <ErrorBoundary />;
-};
-export default ErrorCoustom;

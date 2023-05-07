1e092f2894deb2e9252fc77059152b00ef2ee086
JaeKwan Ahn <52008@naver.com>
2023-05-08 01:17:31 +0900

    Refactor: 차크라 빠진 부분 적용

--- src/pages/DustMap.tsx
+++ src/pages/DustMap.tsx
@@ -1,18 +1,12 @@
-import styled from '@emotion/styled';
 import Map from '@/components/Map';
+import { Box } from '@chakra-ui/react';
 
 const DustMapPage = () => {
   return (
-    <MapContainer>
+    <Box maxWidth="37.5rem" width="100vw" height="100vh">
       <Map />
-    </MapContainer>
+    </Box>
   );
 };
 
 export default DustMapPage;
-
-const MapContainer = styled.div`
-  max-width: 37.5rem;
-  width: 100vw;
-  height: 100vh;
-`;

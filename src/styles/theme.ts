bd9c8fc782aabcd782d12a6c800b6daffb90fdef
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-08 23:14:07 +0900

    feat: theme 색상 정의

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,19 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  styles: {
-    global: () => ({
-      body: {
-        bg: '#53caf2',
-        lineHeight: 1,
-      },
-    }),
+  colors: {
+    DANGER: '#e64746',
+    BAD: '#fda60d',
+    NORMAL: '#03c73c',
+    GOOD: '#30a2ff',
+  },
+  backgroundColors: {
+    DANGER:
+      'linear-gradient(77deg, rgba(255,255,180,1) 0%, rgba(246,104,103,1) 50%, rgba(230,71,70,1) 100%)',
+    BAD: 'linear-gradient(77deg, rgba(255,255,180,1) 0%, rgba(255,189,74,1) 50%, rgba(253,166,13,1) 100%)',
+    NORMAL:
+      'linear-gradient(77deg, rgba(255,255,180,1) 0%, rgba(110,226,144,1) 50%, rgba(3,199,60,1) 100%)',
+    GOOD: 'linear-gradient(77deg, rgba(255,255,180,1) 0%, rgba(83,202,242,1) 50%, rgba(48,162,255,1) 100%)',
   },
 });
 

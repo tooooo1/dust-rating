9b4505e49c4a568d6f95423bbff620205f4b28b5
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-08 23:28:03 +0900

    refactor: color 상수 제거 및 chakra theme로 대체

--- src/utils/constants/map.ts
+++ src/utils/constants/map.ts
@@ -6,10 +6,3 @@ export const COLOR_MARKER_MOUSE_OVER = 'yellow';
 export const COLOR_MARKER_MOUSE_OUT = 'white';
 export const ZINDEX_MARKER_MOUSE_OVER = '100';
 export const ZINDEX_MARKER_MOUSE_OUT = '0';
-
-export const DUST_SCALE_COLOR = {
-  DANGER: '#e64746',
-  BAD: '#fda60d',
-  NORMAL: '#03c73c',
-  GOOD: '#30a2ff',
-};

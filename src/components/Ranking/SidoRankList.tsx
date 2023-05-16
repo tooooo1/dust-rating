0a6799315251347926f1d45e6e5edcf221c86e10
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-16 22:24:16 +0900

    refactor: && 연산자 제거

--- src/components/Ranking/SidoRankList.tsx
+++ src/components/Ranking/SidoRankList.tsx
@@ -24,7 +24,6 @@ const SidoRankList = ({ sortType }: SidoRankListProps) => {
           );
         }
       },
-      refetchOnWindowFocus: false,
       keepPreviousData: true,
       staleTime: 1000 * 60 * 5,
       suspense: true,
@@ -33,18 +32,17 @@ const SidoRankList = ({ sortType }: SidoRankListProps) => {
 
   return (
     <>
-      {sidoDustInfos &&
-        sidoDustInfos.map((sido, sidoIndex) => (
-          <SidoRankItem
-            key={sido.sidoName}
-            rank={sidoIndex + 1}
-            sidoName={sido.sidoName}
-            fineDustScale={sido.fineDustScale}
-            ultraFineDustScale={sido.ultraFineDustScale}
-            fineDustGrade={sido.fineDustGrade}
-            ultraFineDustGrade={sido.ultraFineDustGrade}
-          />
-        ))}
+      {sidoDustInfos?.map((sido, sidoIndex) => (
+        <SidoRankItem
+          key={sido.sidoName}
+          rank={sidoIndex + 1}
+          sidoName={sido.sidoName}
+          fineDustScale={sido.fineDustScale}
+          ultraFineDustScale={sido.ultraFineDustScale}
+          fineDustGrade={sido.fineDustGrade}
+          ultraFineDustGrade={sido.ultraFineDustGrade}
+        />
+      ))}
     </>
   );
 };

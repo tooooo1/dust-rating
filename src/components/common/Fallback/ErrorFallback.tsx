f52ebcb2b54fcafe667660ac346b99cafea67e64
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-19 04:13:24 +0900

    feat: ErrorFallback 컴포넌트 isCenter props 추가

--- src/components/common/Fallback/ErrorFallback.tsx
+++ src/components/common/Fallback/ErrorFallback.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 import AlertBox from '@/components/common/AlertBox';
 import { ROUTE } from '@/utils/constants';
@@ -6,18 +7,43 @@ import { ROUTE } from '@/utils/constants';
 interface ErrorFallbackProps {
   title: string;
   description?: string;
+  isCenter?: boolean;
 }
 
-const ErrorFallback = ({ title, description }: ErrorFallbackProps) => {
+const ErrorFallback = ({
+  title,
+  description,
+  isCenter = false,
+}: ErrorFallbackProps) => {
   const navigate = useNavigate();
 
   return (
-    <AlertBox title={title} description={description}>
-      <Button onClick={() => navigate(ROUTE.HOME)} mt={4}>
-        메인으로 돌아가기
-      </Button>
-    </AlertBox>
+    <Center isCenter={isCenter}>
+      <AlertBox title={title} description={description}>
+        <Button
+          colorScheme="yellow"
+          variant="outline"
+          mt={4}
+          borderWidth={2}
+          bg="#ffffff"
+          opacity={1}
+          borderRadius={20}
+          _hover={{ bg: '#ffffff8f' }}
+          onClick={() => navigate(ROUTE.HOME)}
+        >
+          메인으로 돌아가기
+        </Button>
+      </AlertBox>
+    </Center>
   );
 };
-
 export default ErrorFallback;
+
+const Center = styled.div<{ isCenter: boolean }>`
+  width: ${({ isCenter }) => (isCenter ? null : '100%')};
+  height: ${({ isCenter }) => (isCenter ? '100%' : null)};
+  display: ${({ isCenter }) => (isCenter ? 'flex' : null)};
+  justify-content: ${({ isCenter }) => (isCenter ? 'center' : null)};
+  align-items: ${({ isCenter }) => (isCenter ? 'center' : null)};
+  margin: ${({ isCenter }) => (isCenter ? '0 12rem' : null)};
+`;

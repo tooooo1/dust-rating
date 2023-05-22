32262cb253271ffe3a06ba4466e5e7a4c2622e02
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-23 00:45:34 +0900

    feat: 예보 이미지 추가 및 전환 애니메이션 구현

--- src/components/DustForcast/ForcastInfo.tsx
+++ src/components/DustForcast/ForcastInfo.tsx
@@ -1,5 +1,6 @@
-import { Text, Image } from '@chakra-ui/react';
+import { Text, Box, keyframes } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
+import { AiFillPauseCircle } from 'react-icons/ai';
 import { getDustForcast } from '@/apis/dustForecast';
 
 interface ForcastInfoProps {
@@ -16,6 +17,13 @@ const ForcastInfo = ({ cityName }: ForcastInfoProps) => {
     }
   );
 
+  const animationKeyframes = keyframes`
+  0% { background-image: url(${dustForecast.imageUrl1}); }
+  50% { background-image: url(${dustForecast.imageUrl2}); }
+  100% { background-image: url(${dustForecast.imageUrl3}); }
+`;
+  const animation = `${animationKeyframes} 3s ease infinite`;
+
   return (
     <>
       <Text fontSize={16} fontWeight={400} textAlign="left" lineHeight={1.4}>
@@ -24,7 +32,36 @@ const ForcastInfo = ({ cityName }: ForcastInfoProps) => {
       <Text fontSize={16} fontWeight={400} textAlign="left" lineHeight={1.4}>
         {dustForecast.informCause}
       </Text>
-      <Image src={dustForecast.imageUrl1} alt="대기질 예보 이미지" mt={8} />
+      <Box
+        className="forecast-img"
+        width="100%"
+        height={800}
+        position="relative"
+        animation={animation}
+        mt={8}
+        _hover={{ animationPlayState: 'paused' }}
+      >
+        <Box
+          opacity={0}
+          position="absolute"
+          top={20}
+          right={4}
+          color="#ffffffed"
+          fontSize={50}
+          transition="all 100ms ease-in-out"
+          sx={{
+            '.forecast-img:hover &': {
+              opacity: '1',
+            },
+          }}
+        >
+          <AiFillPauseCircle
+            style={{
+              filter: 'drop-shadow(0px 3px 10px rgba(48, 48, 48, 0.4))',
+            }}
+          />
+        </Box>
+      </Box>
     </>
   );
 };

3d8c56c2bdd94e4bb0f1f2a38ad785005ce32c68
JaeKwan Ahn <52008@naver.com>
2023-05-23 00:30:08 +0900

    Style: NONE 상태의 색상 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -24,7 +24,7 @@ const theme = extendTheme({
     NORMAL:
       'linear-gradient(77deg, rgba(255,255,180,1) 0%, rgba(110,226,144,1) 50%, rgba(3,199,60,1) 100%)',
     GOOD: 'linear-gradient(77deg, rgba(255,255,180,1) 0%, rgba(83,202,242,1) 50%, rgba(48,162,255,1) 100%)',
-    NONE: 'rgba(154, 152, 160, 1)',
+    NONE: '#ffffff8f',
   },
 });
 

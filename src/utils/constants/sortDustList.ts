d28176da9d6388dca49bc2ab48afe0428c00b87c
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-22 19:40:53 +0900

    refactor: dust data 정렬 유틸 함수 구현

---
+++ src/utils/constants/sortDustList.ts
@@ -0,0 +1,16 @@
+import { CityDustInfo, SidoDustInfo, SortType } from '@/types/dust';
+import { FINE_DUST, ULTRA_FINE_DUST } from './dust';
+
+export const sortDustList = <T extends SidoDustInfo | CityDustInfo>(
+  sortType: SortType,
+  data: T[]
+) => {
+  if (sortType === FINE_DUST) {
+    return data?.sort((prev, cur) => prev.fineDustScale - cur.fineDustScale);
+  }
+  if (sortType === ULTRA_FINE_DUST) {
+    return data?.sort(
+      (prev, cur) => prev.ultraFineDustScale - cur.ultraFineDustScale
+    );
+  }
+};

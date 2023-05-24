882c12ca13bb408e75022657ea5f62ea5ade179e
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-25 03:36:33 +0900

    feat: 예보 이미지 크기 수정

--- src/components/DustForecast/ForecastTab.tsx
+++ src/components/DustForecast/ForecastTab.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text, Image as ImageBox } from '@chakra-ui/react';
+import { Flex, Box, Text, Image as ImageWrapper } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { DustForecast } from '@/types/dust';
 
@@ -43,10 +43,11 @@ const ForecastTab = ({ dustForecast }: ForecastTabProps) => {
       >
         {dustForecast.informCause}
       </Text>
-      <Flex justifyContent="center" width="100%" mt={8}>
-        <ImageBox
+      <Flex justifyContent="center" width="100%" mt={20}>
+        <ImageWrapper
           src={imgSrc}
           width={imgSrc === dustForecast.imageSrc ? '84%' : '100%'}
+          transform="scale(1.2)"
           ml={imgSrc === dustForecast.imageSrc ? '0.1rem' : '0'}
           mt={imgSrc === dustForecast.imageSrc ? '0.5rem' : '0'}
           mb={imgSrc === dustForecast.imageSrc ? '0.6rem' : '0'}

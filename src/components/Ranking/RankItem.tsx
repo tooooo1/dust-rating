8956194c63813c467236dd174f359c03b9f45f1c
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-25 17:32:49 +0900

    refactor: Rank 컴포넌트 삭제

--- src/components/Ranking/RankItem.tsx
+++ src/components/Ranking/RankItem.tsx
@@ -1,7 +1,8 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
-import { Rank } from '@/components/common';
+import { DustState } from '@/components/common';
 import { CityDustInfo, SidoDustInfo } from '@/types/dust';
+import { FINE_DUST, ULTRA_FINE_DUST } from '@/utils/constants';
 
 interface RankItemProps {
   rankNumber: number;
@@ -38,7 +39,67 @@ const RankItem = ({
       }}
       onClick={handlePageNavigate}
     >
-      <Rank rank={rankNumber} title={rankTitle} dustFigures={rankInfo} />
+      <Flex flex={1} alignItems="center">
+        <Text
+          as="span"
+          fontSize={{ base: 14, sm: 16 }}
+          fontWeight={400}
+          mr={4}
+          color="#9dadb6"
+          width="8%"
+        >
+          {rankNumber}
+        </Text>
+        <Text
+          as="p"
+          width="30%"
+          fontSize={{ base: 16, sm: 18 }}
+          fontWeight={700}
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow="ellipsis"
+          title={rankTitle.length > 7 ? rankTitle : ''}
+        >
+          {rankTitle}
+        </Text>
+        <Box width="26%" mr={8}>
+          <DustState dustGrade={rankInfo.fineDustGrade} />
+        </Box>
+        <Flex direction="column" justifyContent="center" flexGrow={1}>
+          <Flex justifyContent="space-between" alignItems="center" py={1}>
+            <Text
+              as="p"
+              mr={2}
+              fontSize={{ base: 12, sm: 14 }}
+              fontWeight={500}
+              overflow="hidden"
+              whiteSpace="nowrap"
+              textOverflow="ellipsis"
+            >
+              {FINE_DUST}
+            </Text>
+            <Text as="p" fontSize={{ base: 10, sm: 12 }} fontWeight={800}>
+              {rankInfo.fineDustScale}
+            </Text>
+          </Flex>
+          <Flex justifyContent="space-between" alignItems="center" py={1}>
+            <Text
+              as="p"
+              mr={2}
+              fontSize={{ base: 12, sm: 14 }}
+              fontWeight={500}
+              overflow="hidden"
+              whiteSpace="nowrap"
+              textOverflow="ellipsis"
+            >
+              {ULTRA_FINE_DUST}
+            </Text>
+            <Text as="p" fontSize={{ base: 10, sm: 12 }} fontWeight={800}>
+              {rankInfo.ultraFineDustScale}
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
--- src/components/Ranking/index.ts
+++ src/components/Ranking/index.ts
@@ -1,6 +1,4 @@
-export { default as CityRankItem } from '@/components/Ranking/CityRankItem';
 export { default as CityRankList } from '@/components/Ranking/CityRankList';
 export { default as SelectList } from '@/components/Ranking/SelectList';
-export { default as SidoRankItem } from '@/components/Ranking/SidoRankItem';
 export { default as SidoRankList } from '@/components/Ranking/SidoRankList';
 export { default as SelectTabList } from '@/components/Ranking/SelectTabList';

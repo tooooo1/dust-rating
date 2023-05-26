1cc2084a9ec416e3235ef585f6203196871617cc
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-26 21:28:14 +0900

    fix: 스피너 fallback 제거

--- src/pages/SidoRanking.tsx
+++ src/pages/SidoRanking.tsx
@@ -1,4 +1,4 @@
-import { Center, Flex, Spinner } from '@chakra-ui/react';
+import { Flex, Skeleton } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { NavButton } from '@/components/Nav';
 import {
@@ -12,13 +12,6 @@ import { INIT_SIDO } from '@/utils/constants';
 
 const SidoRanking = () => {
   const sidoDustInfo = useSidoDustInfoQuery(INIT_SIDO);
-
-  if (!sidoDustInfo)
-    return (
-      <Center height="100vh">
-        <Spinner />
-      </Center>
-    );
 
   return (
     <Flex

339e6773dbf1e983b39185456f10fe70f10c989d
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-05-26 21:26:29 +0900

    fix: 도시 변경시 화면 깜빡임 해결

--- src/pages/CityRanking.tsx
+++ src/pages/CityRanking.tsx
@@ -1,30 +1,39 @@
-import { Center, Flex, Spinner } from '@chakra-ui/react';
+import { Box, Center, Flex, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DustFigureBar, DustState, Select } from '@/components/common';
 import { NavButton } from '@/components/Nav/NavButton';
 import {
   CityRankList,
   RankingContent,
   RankingHeader,
 } from '@/components/Ranking';
-import RankingDetail from '@/components/Ranking/RankingDetail';
 import { useSidoDustInfoQuery } from '@/hooks/useDustInfoQuery';
 import theme from '@/styles/theme';
-import { DUST_GRADE, INIT_SIDO, BACKGROUND_ANIMATION } from '@/utils/constants';
+import {
+  DUST_GRADE,
+  INIT_SIDO,
+  BACKGROUND_ANIMATION,
+  FINE_DUST,
+  ULTRA_FINE_DUST,
+  SIDO_NAMES,
+  ROUTE,
+} from '@/utils/constants';
 
 const CityRanking = () => {
+  const navigate = useNavigate();
+
   const { place = INIT_SIDO } = useParams();
   const [grade, setGrade] = useState(0);
 
   const sidoDustInfo = useSidoDustInfoQuery(place);
 
-  if (!sidoDustInfo)
-    return (
-      <Center height="100vh">
-        <Spinner />
-      </Center>
-    );
+  const handleSidoChange = (place: string) => {
+    setGrade((prevGrade) => sidoDustInfo?.fineDustGrade ?? prevGrade);
+
+    navigate(`${ROUTE.RANKING}/${place}`);
+  };
 
   return (
     <Flex
@@ -47,11 +56,51 @@ const CityRanking = () => {
         }}
       />
       <RankingHeader dataTime={sidoDustInfo?.dataTime} />
-      <RankingDetail
-        place={place}
-        setGrade={setGrade}
-        dustInfo={sidoDustInfo}
-      />
+      <Box
+        maxWidth="30rem"
+        width={{ base: '80%', sm: '80%' }}
+        margin="0 auto"
+        position="relative"
+        borderTopRadius={10}
+        textAlign="center"
+        bg="rgba(255, 255, 255, 0.6)"
+        boxShadow="0 4px 30px rgba(0, 0, 0, 0.1)"
+        backdropFilter="blur(7px)"
+        p={{ base: 4, sm: 6 }}
+      >
+        <Text
+          as="p"
+          fontSize={{ base: 20, sm: 22 }}
+          fontWeight={700}
+          mb={{ base: 2, sm: 4 }}
+        >
+          {place}
+        </Text>
+        <Box position="absolute" top={4} left={4}>
+          <Select options={SIDO_NAMES} onClick={handleSidoChange} />
+        </Box>
+        <Text
+          as="div"
+          mt="1rem"
+          fontSize={{ base: 14, sm: 15 }}
+          color="#4d4d4d"
+        >
+          현재의 대기질 지수는
+        </Text>
+        <Center my={5}>
+          <DustState dustGrade={sidoDustInfo?.fineDustGrade || 0} />
+        </Center>
+        <DustFigureBar
+          kindOfDust={FINE_DUST}
+          scale={sidoDustInfo?.fineDustScale}
+          grade={sidoDustInfo?.fineDustGrade}
+        />
+        <DustFigureBar
+          kindOfDust={ULTRA_FINE_DUST}
+          scale={sidoDustInfo?.ultraFineDustScale}
+          grade={sidoDustInfo?.ultraFineDustGrade}
+        />
+      </Box>
       <RankingContent
         backgroundColor={
           theme.colors[DUST_GRADE[sidoDustInfo?.fineDustGrade || grade]]

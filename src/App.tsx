bd8df182d33d5816fa5406710d1d06448c8828fb
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-06-29 21:07:16 +0900

    feat(App): DustMapPage 컴포넌트 errorBoundary 적용

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import {
 import { ROUTE } from '@/utils/constants';
 import SortContextProvider from './store/sort';
 import theme from './styles/theme';
+import { fallbackSize } from './types/error';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -51,7 +52,21 @@ const router = createBrowserRouter(
           </AsyncBoundary>
         }
       />
-      <Route path={ROUTE.DUST_MAP} element={<DustMapPage />} />
+      <Route
+        path={ROUTE.DUST_MAP}
+        element={
+          <AsyncBoundary
+            rejectFallback={
+              <ErrorFallback
+                size={fallbackSize.FULL}
+                errorMessage="지도를 불러오지 못했어요."
+              />
+            }
+          >
+            <DustMapPage />
+          </AsyncBoundary>
+        }
+      />
       <Route path="*" element={<Navigate replace to={ROUTE.HOME} />} />
     </Route>
   )

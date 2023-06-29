6558034111825d8f319f11946baa87c22b88c7eb
JaeKwan Ahn <52008@naver.com>
2023-06-30 02:10:40 +0900

    Feat: 정렬 기준 추가

--- src/store/sort.tsx
+++ src/store/sort.tsx
@@ -5,18 +5,22 @@ import {
   MouseEvent,
   PropsWithChildren,
 } from 'react';
-import type { DustType } from '@/types/dust';
+import type { DustType, SortType } from '@/types/dust';
 import { FINE_DUST, ULTRA_FINE_DUST } from '@/utils/constants';
 
 interface SortContextState {
   dustType: DustType;
+  sortType: SortType;
   setDustType: (e: MouseEvent<HTMLButtonElement>) => void;
+  setSortType: (e: MouseEvent<HTMLButtonElement>) => void;
 }
 
 const initialSortState: SortContextState = {
   dustType: FINE_DUST,
+  sortType: 'ASCENDING',
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setDustType: () => {},
+  setSortType: () => {},
 };
 
 const SortContext = createContext<SortContextState>(initialSortState);
@@ -25,6 +29,7 @@ export const useSort = () => useContext(SortContext);
 
 const SortContextProvider = ({ children }: PropsWithChildren) => {
   const [dustType, setDustType] = useState<DustType>(FINE_DUST);
+  const [sortType, setSortType] = useState<SortType>('ASCENDING');
 
   const handleDustTypeChange = (e: MouseEvent<HTMLButtonElement>) => {
     setDustType(
@@ -32,9 +37,20 @@ const SortContextProvider = ({ children }: PropsWithChildren) => {
     );
   };
 
+  const handleSortTypeChnage = (e: MouseEvent<HTMLButtonElement>) => {
+    setSortType(
+      e.currentTarget.value === 'ASCENDING' ? 'ASCENDING' : 'DESCENDING'
+    );
+  };
+
   return (
     <SortContext.Provider
-      value={{ dustType, setDustType: handleDustTypeChange }}
+      value={{
+        dustType,
+        setDustType: handleDustTypeChange,
+        sortType,
+        setSortType: handleSortTypeChnage,
+      }}
     >
       {children}
     </SortContext.Provider>

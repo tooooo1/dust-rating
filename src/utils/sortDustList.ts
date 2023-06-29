009544f8c3ed626516ebf07e2947975d0d40e297
JaeKwan Ahn <52008@naver.com>
2023-06-30 02:46:12 +0900

    Refactor: 정렬 기준에 오름차순 내림 차순 추가

--- src/utils/sortDustList.ts
+++ src/utils/sortDustList.ts
@@ -1,12 +1,23 @@
-import type { CityDustInfo, SidoDustInfo, DustType } from '@/types/dust';
+import type {
+  CityDustInfo,
+  SidoDustInfo,
+  DustType,
+  SortType,
+} from '@/types/dust';
 import { FINE_DUST } from './constants/dust';
+import { ASCENDING } from './constants';
 
 export const sortDustList = <T extends SidoDustInfo | CityDustInfo>(
   DustType: DustType,
+  sortType: SortType,
   data: T[]
 ) => {
   const scaleKey =
     DustType === FINE_DUST ? 'fineDustScale' : 'ultraFineDustScale';
 
-  return data.sort((a, b) => a[scaleKey] - b[scaleKey]);
+  return data.sort((a, b) => {
+    const A = a[scaleKey];
+    const B = b[scaleKey];
+    return sortType === ASCENDING ? A - B : B - A;
+  });
 };

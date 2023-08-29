64c79adae59e1887e0b452407d1ac1e920f62ffc
JaeKwan Ahn <52008@naver.com>
2023-08-29 11:22:57 +0900

    Feat: 에러 메시지 추가

--- src/utils/constants/message.ts
+++ src/utils/constants/message.ts
@@ -3,4 +3,5 @@ export const ERROR_MESSAGE = {
   NO_RANKING_DATA: '지역별 미세먼지 정보를 불러오지 못했어요.',
   NO_FORECAST_DATA: '예보 정보를 불러오지 못했어요.',
   NO_MAP_DATA: '지도를 불러오지 못했어요.',
+  NO_CITY_DATA: '동네 정보를 받아오지 못했어요',
 } as const;
